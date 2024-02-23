ac85280ffbf619919d7382f83ab04a76e1cb387b
Minakshi Patil <pminu1972000@gmail.com>
2024-02-23 19:16:36 +0530

    updated API

--- hackethon.js
+++ hackethon.js
@@ -4,19 +4,19 @@ document.addEventListener('DOMContentLoaded',    () => {
     
 
     async  function fetchnews(keyword) {
-        const key = 'd9e4eaf0938a4249b3a6eff56f2ad194';
+        const key = '7d1b5cca93e34c39a5ce1e63080bac27';
         const country = 'in';
         //const URL = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&apiKey=${key}&sortBy=popularity`
         let URL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${key}`
         try {
             if (keyword) {
-                 URL = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&apiKey=${key}`
-                 
+                 //URL = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&apiKey=${key}`
+                  URL =  `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${key}`
                 }
             const response = await fetch(URL);
             const newsdata = await response.json();
             if (response.ok && newsdata.articles) {
-                displayNews(newsdata.articles.slice(0, 10));
+                displayNews(newsdata.articles.slice(0 , 15));
               
             } else {
                 console.error('Error fetching news:', newsdata.message || 'Unexpected response');
@@ -27,27 +27,36 @@ document.addEventListener('DOMContentLoaded',    () => {
         }
     }
     function displayNews(art) {
-        newsContainer.innerHTML = '';
-        const rightArrow = '\u2192';
+       newsContainer.innerHTML = '';
+        //const rightArrow = '\u2192';
+     
 
         art.forEach(article => {
 
-            const card = document.createElement('div');
+           const card = document.createElement('div');
             card.className = 'News-card';
             const image = document.createElement('img');
+            if(article.urlToImage){    // image checking 
             image.src = article.urlToImage;
             image.alt = article.title;
+             } 
+             else{
+                 image.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvkRa0hCi5HlS1GjN3tJM3uCLlSX3RcHpYNMMhMkXRXw&s'
+             }
+           
             const heading = document.createElement('h3');
             heading.innerHTML = article.title;
             const description = document.createElement('p');
             description.textContent = article.description;
             const btn = document.createElement('button')
-            btn.className = 'Read-more';
-            const addtext = document.createTextNode(`Read Full article ${rightArrow}`)
-            btn.addEventListener('click', ()=>{
-                Read_full_news(article.url);
+            btn.className = 'Read-more';   
+           const addtext = document.createTextNode( 'Read full article'  )
+            addtext.className = "Right-arrow"
+           card.addEventListener('click', ()=> {
+                  Read_full_news (article.url);
             });            
-            btn.appendChild(addtext);
+            
+           btn.appendChild(addtext);
             card.appendChild(image);
             card.appendChild(heading);
             card.appendChild(description);
@@ -55,37 +64,34 @@ document.addEventListener('DOMContentLoaded',    () => {
             newsContainer.appendChild(card);
             
 
-        }); 
+        });  
 
-       // update_header(search_input.value.trim());
-        
+       // update_header(search_input.value.trim());    
 
-       async function Read_full_news (url){
+       function Read_full_news (url){    // full news read function attached to button
         try{
             window.open(url , '_blank');
         }
         catch (error){
             console.error('Error opening article:', error.message || 'Unexpected response');
         }
-
         }
-       
-      
-        
+         
     }
 
     async function update_header(keyword) {
+
         if(search_input){
-          paraDiv.textContent = `${keyword}`
-        }
-       
+              paraDiv.innerHTML= `${keyword}` 
+        }  
     }
      
-   async function news () {
+   async function news () {    // initial load
        await fetchnews('');
 } 
 news();
-    
+
+
 
     const search_button = document.getElementById('go_btn');
     const search_input = document.getElementById('input');
@@ -93,9 +99,14 @@ news();
     search_button.addEventListener('click', async () => {
         const trim_keyword = search_input.value.trim();
         const keyword = encodeURIComponent(trim_keyword);
+        if(trim_keyword === ' '){
+            search_button.disabled = true
+        } else{
+            search_button.disabled = false
+        }
 
         await fetchnews(keyword);
-        await update_header(search_input.value)
+       await update_header(search_input.value)
               
        
     });
